15c1295a8534dc87a8e08693ba75e566176db748
oralaner <alexisporiel@gmail.com>
2025-12-14 18:53:32 +0100

    Fix homepage

--- grand-line-v2/app/features/HomeTab.js
+++ grand-line-v2/app/features/HomeTab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { formatChronoLong, getRankInfo } from '../utils/gameUtils';
 import DailyQuestsWidget from './DailyQuestsWidget';
-import EnergyBar from './EnergyBar'; // ✅ On importe ton composant EnergyBar
+import EnergyBar from './components/EnergyBar'; // ✅ On importe ton composant EnergyBar
 
 const HomeTab = ({ joueur, statsTotales, expeditionChrono, topJoueurs, topEquipages, onNavigate, theme, monEquipage, membresEquipage }) => { 
     
